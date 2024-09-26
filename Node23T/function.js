85d820b9435ff4dd8e5fecf84a92738b9dc1c45e
Aldilaaaanr <135348078+Aldilaaaanr@users.noreply.github.com>
2024-09-26 12:52:47 +0700

    PBP_Aldila nur azizah_TI23T
    nim : 20230040041

---
+++ Node23T/function.js
@@ -0,0 +1,17 @@
+//traditionally functiuon
+
+function add2Number(num1,num2){
+
+    let result = num1 + num2
+    return result
+}
+
+//Arrow function
+
+let divide2Number = (num1,num2) => {
+    let result = num1 / num2
+    return result
+}
+
+console.log(add2Number(10,50));
+console.log(divide2Number(50,10));
---
+++ Node23T/hello.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+
+var firstname = "Aldila"
+var lastname = "nura"
+
+//My name is Aldila nura
+console.log("My name is " + firstname + " " + lastname);
+
+//string literal
+console.log(`My name is ${firstname} ${lastname}`);
+
+var num1 = 50
+var num2 = 100
+
+console.log(`Multiply Result is ${num1 * num2}`);
+
+let num3 = "10"
+var num1 = 100
+let age;
+
+console.log(typeof num3);
+console.log(typeof num1);
+console.log(typeof age);
+
+
