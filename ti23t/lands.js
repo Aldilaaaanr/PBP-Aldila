f9f8f1159880b79335c180560ffd34b0019e2c95
Aldilaaaanr <135348078+Aldilaaaanr@users.noreply.github.com>
2024-10-17 13:30:48 +0700

    Delete ti23t directory

--- ti23t/lands.js
+++
@@ -1,9 +0,0 @@
-let {checkCondition} = require('./checkweather')
-
-const weatherData = {
-    temperature : 26,
-    humidity : 60,
-    winSpeed : 10
-}
-let result = checkCondition(weatherData)
-console.log(result);
